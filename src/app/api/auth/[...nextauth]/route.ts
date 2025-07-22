2ea27c22f88a1933eedba4507cd4d2f6288d2114
royanish123 <anish.anandroy@gmail.com>
2025-07-23 01:07:19 +0530

    feat(auth): configure NextAuth with CredentialsProvider and JWT strategy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
-const Handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions);
 
-export { Handler as GET , Handler as POST}  
+export { handler as GET, handler as POST };
